46d775cff592fc29d773d1d11b7f242f7ba8adde
khalifa <mednaceur199530@gmail.com>
2022-09-26 09:28:21 +0100

    make sign Up

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -3,7 +3,12 @@
 namespace App\Form;
 
 use App\Entity\User;
+
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType as TypeTextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,11 +16,13 @@ class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-        ;
+        $builder ->add('email', EmailType::class)
+       
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => ['label' => 'Password'],
+                'second_options' => ['label' => 'Confirm Password']
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
